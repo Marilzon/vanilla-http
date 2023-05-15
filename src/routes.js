2e9488fb734ada1021636b1a788a0995d81f973e
Marilzon <maxmaril@hotmail.com>
2023-05-15 16:34:15 -0300

    feature: some routes

--- src/routes.js
+++ src/routes.js
@@ -36,4 +36,78 @@ export const routes = [
       return response.writeHead(201).end();
     },
   },
+  {
+    method: "GET",
+    path: buidRoutePath("/tasks"),
+    handler: (request, response) => {
+      const { search } = request.query;
+
+      const tasks = database.select("tasks", {
+        title: search,
+        description: search,
+      });
+
+      return response.end(JSON.stringify(tasks));
+    },
+  },
+  {
+    method: "PUT",
+    path: buidRoutePath("/tasks/:id"),
+    handler: (request, response) => {
+      const { id } = request.params;
+      const { title, description } = request.body;
+      let missingProps = [];
+
+      if (!title) missingProps.push("title");
+      if (!description) missingProps.push("description");
+
+      if (missingProps.length) {
+        const missingPropsMessage = `The following props are missing: ${missingProps.join(
+          ","
+        )}`;
+        return response.status(400).send(missingPropsMessage);
+      }
+
+      const [task] = database.select("tasks", id, {
+        title,
+        description,
+        updated_at: new Date().toISOString().slice(0, 10),
+      });
+
+      return response.writeHead(204).end();
+    },
+  },
+  {
+    method: "DELETE",
+    path: buidRoutePath("/tasks/:id"),
+    handler: (request, response) => {
+      const { id } = request.params;
+      const [task] = database.select("tasks", { id });
+
+      if (!task) return response.writeHead(404).end;
+
+      database.delete("tasks", id);
+
+      return response.writeHead(204).end();
+    },
+  },
+  {
+    method: "PATCH",
+    path: buidRoutePath("/tasks:id/complete"),
+    handler: (request, response) => {
+      const { id } = request.params;
+      const [task] = database.select("tasks", { id });
+
+      if (!task) return response.writeHead(404).end();
+
+      const taskCompleted = !!task.completed_at;
+      const completed_at = taskCompleted
+        ? null
+        : new Date().toISOString().slice(0, 10);
+
+      database.update("tasks", id, { completed_at });
+
+      return response.writeHead(204).end();
+    },
+  },
 ];
